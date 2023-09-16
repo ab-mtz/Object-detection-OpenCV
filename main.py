6374c145f35dfd8ac9f82e2566490314ce3b1fd5
ab-mtz <bmtzs@me.com>
2023-09-16 09:59:49 +0200

    opens camera

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from cvlib.object_detection import draw_bbox
 # from gtts import gTTS
 # from playsound import playsound
 
-# Code to open a picture
+# Code to open webcam
 video = cv2.VideoCapture(0)
 labels = []
 
 while True:
     ret, frame = video.read()
     bbox, label, conf = cv.detect_common_objects(frame)
-    output_image = draw.bbox(frame, bbox, label, conf)
+    output_image = draw_bbox(frame, bbox, label, conf)
 
     cv2.imshow("object detection", frame)
 
     for item in label:
         if item in labels:
             pass
+        else:
+            labels.append(item)
+
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
-vid.realease()
+print(labels)
 
-cv2.destroyAllWindows()
+# vid.realease()
+
+# cv2.destroyAllWindows()
